9fd3264ddee4d1de855f9be065a955291a64d00b
anrui2032 <anrui2032@gmail.com>
2023-08-12 10:36:45 +0000

    oxford: Remove mokee stuffs
    Change-Id: I2025682b8460ad3f4456db8568d6d83b6d69be95

--- lineage_oxford.mk
+++ lineage_oxford.mk
@@ -25,9 +25,6 @@ $(call inherit-product, device/smartisan/oxford/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.mk.maintainer=XiNGRZ
-
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_oxford
 PRODUCT_BRAND := SMARTISAN
